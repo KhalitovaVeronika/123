1db51d04ff47cfcd6cd8a495d03f4f5866db1b1a
Veronika Khalitova <veronikabaranovak@yandex.ru>
2022-06-27 20:38:41 +0300

    public

---
+++ README.md
@@ -0,0 +1,14 @@
+# Проект: Место
+
+### Обзор
+
+- Figma
+- Картинки
+
+**Figma**
+
+- [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
+
+В проектной работе 4 реализовано одно диалоговое окно из макета — «Редактировать профиль». В нём два поля: «Имя» и «О себе», а также имеется кнопка «Сохранить»/
+
+Сайт размещен по адресу https://khalitovaveronika.github.io/mesto/index.html
---
+++ script/script.js
@@ -0,0 +1,31 @@
+const popup = document.querySelector('.popup');
+const buttonEdit = document.querySelector('.profile__edit-button');
+const buttonClose = document.querySelector('.popup__close-button');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+
+let nameInput = document.querySelector('#name');
+let descriptionInput = document.querySelector('#description');
+let formElement = document.querySelector('.form');
+
+function openPopup(){
+  popup.classList.remove('popup_hidden');
+  nameInput.value = profileName.textContent;
+  descriptionInput.value = profileDescription.textContent;
+}
+
+function closePopup(){
+  popup.classList.add('popup_hidden');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+  closePopup();
+}
+
+buttonEdit.addEventListener('click', openPopup);
+buttonClose.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
+
